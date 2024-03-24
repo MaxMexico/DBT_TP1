83cc765808a2fc1d80664ed9d5dee8ebfada04c7
MaxMexico <142561188+MaxMexico@users.noreply.github.com>
2024-03-24 12:12:18 +0100

    Add files via upload

---
+++ DBT_TP1/models/marts/daily_sales_revenue_by_category.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+SELECT
+    s.sale_date,
+    p.category,
+    SUM(s.quantity * p.price) AS daily_revenue
+FROM {{ ref('stg_sales') }} s
+JOIN {{ ref('stg_products') }} p ON s.product_id = p.product_id
+GROUP BY s.sale_date, p.category;
